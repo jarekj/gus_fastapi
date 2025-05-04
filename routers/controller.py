d039e379ab09723abbbeeceb4d645489fc947c7a
jarekj <jjankowski1@gmail.com>
2025-05-04 14:53:10 +0200

    add krs2regon, reutrn with PlainTextResponse to 2regon endpoitn

--- routers/controller.py
+++ routers/controller.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException
+from fastapi.responses import PlainTextResponse
 from services.gus_service import get_company_details_by_regon, get_company_details_by_nip, get_company_details_by_krs
 from utils import validator
 
@@ -6,7 +7,6 @@ router = APIRouter()
 
 
 @router.get("/regon/{number}", response_model=list)
-
 async def get_details_by_regon(number: str):
     if validator.is_regon_valid(number) == False:
         raise HTTPException(status_code=400, detail="Invalid REGON number")
@@ -17,8 +17,7 @@ async def get_details_by_regon(number: str):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get("/regon2nip/{number}", response_model=str)
-
+@router.get("/regon2nip/{number}", response_class=PlainTextResponse)
 async def get_details_by_regon(number: str):
     if validator.is_regon_valid(number) == False:
         raise HTTPException(status_code=400, detail="Invalid REGON number")
@@ -30,7 +29,6 @@ async def get_details_by_regon(number: str):
 
 
 @router.get("/nip/{number}", response_model=list)
-
 async def get_details_by_nip(number: str):
     if validator.is_nip_valid(number) == False:
         raise HTTPException(status_code=400, detail="Invalid NIP number")
@@ -41,8 +39,7 @@ async def get_details_by_nip(number: str):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get("/nip2regon/{number}", response_model=str)
-
+@router.get("/nip2regon/{number}", response_class=PlainTextResponse)
 async def get_details_by_nip(number: str):
     if validator.is_nip_valid(number) == False:
         raise HTTPException(status_code=400, detail="Invalid NIP number")
@@ -53,7 +50,6 @@ async def get_details_by_nip(number: str):
         raise HTTPException(status_code=500, detail=str(e))
     
 @router.get("/krs/{number}", response_model=list)
-
 async def get_details_by_krs(number: str):
     if validator.is_krs_valid_length(number) == False:
         raise HTTPException(status_code=400, detail="Invalid KRS number length, should be 10 digits")
@@ -63,3 +59,12 @@ async def get_details_by_krs(number: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
+@router.get("/krs2regon/{number}", response_class=PlainTextResponse)
+async def get_details_by_krs(number: str):
+    if validator.is_krs_valid_length(number) == False:
+        raise HTTPException(status_code=400, detail="Invalid KRS number length, should be 10 digits")
+    try:
+        company_details = get_company_details_by_krs(number)
+        return company_details[0]['Regon']
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
